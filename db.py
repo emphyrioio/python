a1b38f1f726e9c09e5ea2882013797e135afb4f2
RB <redabey82@gmail.com>
2023-01-16 18:01:27 +0100

    commit

--- db.py
+++ db.py
@@ -3,9 +3,11 @@ import textwrap
 
 driver="{/opt/microsoft/msodbcsql18/lib64/libmsodbcsql-18.1.so.2.1}"
 server="SR12-PROD-78162"
+database='db.INOT'
 conString = textwrap.dedent(f"""\
             Driver={driver};
             Server={server};
+            Database={database};
             Trusted_Connection=yes;
             """)
 conn = pyodbc.connect(conString)
