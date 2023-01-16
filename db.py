14b70eebba7948221282213e4ff9a28053c45218
RB <redabey82@gmail.com>
2023-01-16 18:05:45 +0100

    commit

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import pyodbc
 import textwrap
 
 driver="{/opt/microsoft/msodbcsql18/lib64/libmsodbcsql-18.1.so.2.1}"
-server="SR12-PROD-78162"
+server="TCP:SR12-PROD-78162"
 database='db.INOT'
 conString = textwrap.dedent(f"""\
             Driver={driver};
