6d517e00a3c65f1658240617be935a88fb9801d7
Réda BEY <redabey82@gmail.com>
2023-01-20 19:33:38 +0100

    commit

---
+++ Project MVC Tkinter model/model.py
@@ -0,0 +1,12 @@
+class Model:
+    def __init__(self) -> None:
+        pass
+
+    def add(self) -> None:
+        pass
+
+    def delete(self) -> None:
+        pass
+
+    def get(self):
+        pass
